f1e177da6f094eaf23e3dae8b7851ca198d9b75b
Dnish123 <141507377+Dnish123@users.noreply.github.com>
2024-12-11 17:46:53 +0530

    5.sql

--- 5.sql
+++ 5.sql
@@ -1,14 +1,16 @@
+-- Query to convert usd in rupees inr to get a estimate of BO Collection
 Select*,
 IF(currency='USD',revenue*77,revenue) as revenues_inr
 from moviesdb.financials;
 Select*, ((revenue-budget) )as profit,
             (revenue-budget) *100/budget as profit_pct
-            from moviesdb.financials;
--- Select*,
--- Case 
--- 	when unit="Billions" then revenue*1000
--- 	when unit="Thousands" then revenue/1000
--- 	   when unit="millions" then revenue
--- End as revenue_mln
--- from moviesdb.financials;
---        
+from moviesdb.financials;
+-- Query to conveert billions into millions dollar
+Select*,
+Case 
+	when unit="Billions" then revenue*1000
+	when unit="Thousands" then revenue/1000
+	   when unit="millions" then revenue
+End as revenue_mln
+from moviesdb.financials;
+       
