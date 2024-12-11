80f1cfba6a729e689ebe752f469b04c7904539fb
Dnish123 <141507377+Dnish123@users.noreply.github.com>
2024-12-11 17:26:49 +0530

    Add files via upload

---
+++ 1.sql
@@ -0,0 +1,12 @@
+SELECT *  FROM movies where title like  "%america%" ;
+Select * from movies where studio =" ";
+select * from movies where industry ="Bollywood";
+SELECT *  FROM movies where title like  "Thor%" ;
+Select distinct industry from movies;
+Select* from moviesdb.movies;
+Select * from movies where release_year ="1972";
+Select * from movies where imdb_rating ="9%";
+Select release_year, title from movies  where studio like "%Marvel Studios%";
+Select * from movies  where title like "%Avenger%"; 
+Select release_year from movies where title like "%The Godfather%";
+Select distinct title,studio from movies where industry ="Bollywood";
